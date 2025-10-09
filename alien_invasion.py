83de6cc992b9067f3097cb02e33d62f9dd373cbc
Rocio Burgos <rocioburgos00@gmail.com>
2025-10-09 00:23:41 -0300

    Right left movimientos

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,21 +15,36 @@ class AlienInvasion:
         
         self.ship = Ship(self)
 
+
+
     def run_game(self):
         while True:
-             self._check_events()
-             self._update_screen()
-
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+ 
     #Metodos helpers
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             pygame.display.flip()
+
+
 
 #Fuera de la clase
 if __name__ == '__main__':
